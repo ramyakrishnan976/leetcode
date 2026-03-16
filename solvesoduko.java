2fd228119d87740fbb3c0cf8b8618ab5e92c35f9
RAMYA KRISHNAN P <ramyakrishnan976@gmail.com>
2026-03-16 16:33:59 +0530

    Create solvesoduko.java
    Write a program to solve a Sudoku puzzle by filling the empty cells.
    
    A sudoku solution must satisfy all of the following rules:
    
    Each of the digits 1-9 must occur exactly once in each row.
    Each of the digits 1-9 must occur exactly once in each column.
    Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
    The '.' character indicates empty cells.

---
+++ solvesoduko.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c; // Place the number
+
+                            if (backtrack(board)) return true; // Move to next cell
+
+                            board[i][j] = '.'; // Undo (Backtrack)
+                        }
+                    }
+                    return false; // No number works here, go back
+                }
+            }
+        }
+        return true; // All cells filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+            // Check column
+            if (board[i][col] == c) return false;
+            // Check 3x3 sub-box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
