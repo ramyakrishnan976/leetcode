273fd8158d48142a4f10b1521f62c711c89fc569
RAMYA KRISHNAN P <ramyakrishnan976@gmail.com>
2026-04-30 11:48:21 +0530

    Create merge interval.java

---
+++ merge interval.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+
+        // Step 1: Sort by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        
+        // Start with the first interval
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
+
+        for (int[] nextInterval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = nextInterval[0];
+            int nextEnd = nextInterval[1];
+
+            if (nextStart <= currentEnd) { 
+                // Overlap: update the end of the current interval
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else { 
+                // No overlap: move to the next interval and add to list
+                currentInterval = nextInterval;
+                merged.add(currentInterval);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
