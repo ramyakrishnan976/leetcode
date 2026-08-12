07e437904f919756471fe9363b781b99e1469bda
RAMYA KRISHNAN P <ramyakrishnan976@gmail.com>
2026-08-12 14:28:38 +0530

    Create squareroot(x).c

---
+++ squareroot(x).c
@@ -0,0 +1,23 @@
+int mySqrt(int x) {
+    if (x < 2) {
+        return x;
+    }
+    
+    int left = 1;
+    int right = x / 2;
+    int ans = 0;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        // Division avoids integer overflow for 2^31 - 1
+        if (mid <= x / mid) {
+            ans = mid;     
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+    
+    return ans;
+}
